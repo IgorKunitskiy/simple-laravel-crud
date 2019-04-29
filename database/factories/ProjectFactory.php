b10aacbc9f3b6038bec3cb27727822ddb16bb978
Ihor Kynitsky <igor.kynitsky2@gmail.com>
2019-04-29 13:06:49 +0300

    Seeding functionality

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(App\Project::class, function (Faker $faker) {
+
+    $statuses = ['planned', 'running', 'on hold', 'finished', 'cancel'];
+
+    return [
+        'name' => $faker->realText(20),
+        'description' => $faker->realText(200),
+        'status' => $statuses[mt_rand(0, 4)],
+        'deleted' => mt_rand(0, 1),
+    ];
+});
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $users = factory(App\Client::class, 1000)->create();
+        $users = factory(App\Project::class, 1000)->create();
+
         // $this->call(UsersTableSeeder::class);
+        // $this->call(ClientsTableSeeder::class);
+        // $this->call(ProjectsTableSeeder::class);
     }
 }
